ea76723dc8a5aa9f3470cae4277c7263e92018bc
imlyzh <enterindex@gmail.com>
2022-11-01 19:06:15 +0800

    update bytecode

--- src/bytecodes.rs
+++ src/bytecodes.rs
@@ -4,17 +4,32 @@
 
 #[derive(Debug)]
 pub enum SFbytecode {
-  Matchc(u8),
   Accept,
-  Reject
+  Reject,
+  Match1c(u8),
+  Match2c([u8; 2]),
+  Match3c([u8; 3]),
+  Match4c([u8; 4]),
+  TryCycle(usize),          // loop end address(label)
+  Fork2(usize),             // branch 2 address(label)
+  // Fork(Vec<usize>),
+  // StartCountCycle(usize),   // cycle count
+  // ComplatedOnceCycle(usize),// loop start address(label)
 }
-
 
 #[derive(Debug)]
 #[repr(u8)]
 #[allow(non_camel_case_types)]
 pub enum UnwarpedFbytecode {
-  matchc,
   accept,
-  reject
+  reject,
+  match1c,
+  match2c,
+  match3c,
+  match4c,
+  try_cycle,
+  fork2,
+  // fork,
+  // start_count_cycle,
+  // complated_once_cycle,
 }
